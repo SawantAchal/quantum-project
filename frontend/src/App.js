9f191b78bc1a442ce173f6acc5b74aaedaa65845
SawantAchal <achals1718@gmail.com>
2024-07-31 04:20:46 +0530

    pushed frontend for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { Navigate, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
+  const token = localStorage.getItem('token');
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path='/' element={token ? <Navigate to='dashboard'/> : <Login/>}/>
+      <Route path='/dashboard'  element={token ? <Dashboard/> : <Navigate to='/'/>}/>
+    </Routes>
   );
 }
 
